a472f100ae76161ae22c1856152f749213b644e2
Ferdryan Cen <ferdryancen98@gmail.com>
2021-08-06 21:18:28 +0700

    Fix Close
    Fix Close

--- src/handler/deal.js
+++ src/handler/deal.js
@@ -1,8 +1,11 @@
 const api = require('../api');
 
 const getAllDeals = async (request, h) => {
-  const response = await api.getDeals(request, h);
-  return response;
+  const response = await api.getDeals(request.query);
+  return h.response({
+    status: 'OK',
+    data: response,
+  });
 };
 
 module.exports = getAllDeals;
